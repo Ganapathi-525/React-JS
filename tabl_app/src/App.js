cd7d7411d5bc5ec07d6a064ffbc25ba14c79b6ef
Ganapathi-525 <ganapathibakthula@gmail.com>
2023-11-28 15:27:32 +0530

    adding react apps

---
+++ tabl_app/src/App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+function gana(){
+
+  return(
+    
+    <table  >
+    <thead >
+      <tr >
+        <td >Vanilla js advantages and disadvantages
+        </td>
+      </tr>
+    </thead>
+    <tbody style={{color:"blue"}}>
+      <tr style={{colspan:"2"}}>
+        <td>Advantages  of plain JS::</td>
+        <td>Disadvantages of plain JS:</td>
+      </tr>
+
+      <tr>
+        <td><ul><li>Lightweight</li></ul></td>
+        <td><ul><li>Development Time</li></ul></td>
+      </tr>
+      <tr>
+        <td><ul><li>Full Control</li></ul></td>
+        <td><ul><li>Maintenance Complexity</li></ul></td>
+      </tr>
+      <tr>
+        <td><ul><li>Learning the Fundamentals</li></ul></td>
+        <td><ul><li>Cross-browser Compatibility</li></ul></td>
+      </tr>
+      <tr>
+        <td><ul><li>Performance Optimization</li></ul></td>
+        <td><ul><li>Limited Developer Ecosystem</li></ul></td>
+      </tr>
+      <tr>
+        <td><ul><li></li></ul></td>
+        <td><ul><li>Reinventing the Wheel</li></ul></td>
+      </tr>
+      <tr>
+   
+      </tr>
+    </tbody>
+  </table>
+  
+  )
+}
+
+export default gana;
